9b82ead74d1d68190da5abc0c90a22e02fb865cc
Megan <megan.lkm@gmail.com>
2018-01-31 16:37:23 -0600

    do not save hosts that are unreachable via ldap

--- authconfig/files/dclocator.py
+++ authconfig/files/dclocator.py
@@ -1,13 +1,38 @@
+import socket
 from sys import argv
+from telnetlib import Telnet
+
 import srvlookup
 
-names = ['ldap', 'kerberos']
-domain = argv[1]
-servers = []
+NAMES = ['ldap']
 
-for name in names:
-    discovered = srvlookup.lookup(name, domain=domain)
+
+def get_ip(hostname):
+    return socket.gethostbyname(hostname)
+
+
+def ldap_ping(ip):
+    try:
+        Telnet(ip, 389, 5)
+    except:
+        return False
+    return True
+
+
+def main():
+    domain = argv[1]
+    discovered = []
+    servers = []
+
+    for name in NAMES:
+        for thing in srvlookup.lookup(name, domain=domain):
+            if ldap_ping(get_ip(thing.host)):
+                discovered.append(thing)
+
     servers.extend([srv.host for srv in discovered])
+    servers = sorted(list(set(servers)))
+    print(','.join(servers))
 
-servers = sorted(list(set(servers)))
-print(','.join(servers))
+
+if __name__ == '__main__':
+    main()
