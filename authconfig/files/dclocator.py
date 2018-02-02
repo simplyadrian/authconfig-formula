336b066e7b69f19b2fdbb295c3dd377929709e19
Megan <megan.lkm@gmail.com>
2018-02-02 12:50:57 -0600

    return x number of hosts with the shortest ping times

--- authconfig/files/dclocator.py
+++ authconfig/files/dclocator.py
@@ -1,4 +1,6 @@
+import re
 import socket
+from subprocess import check_output
 from sys import argv
 from telnetlib import Telnet
 
@@ -13,25 +15,53 @@ def get_ip(hostname):
 
 def ldap_ping(ip):
     try:
-        Telnet(ip, 389, 5)
+        Telnet(ip, 389, 3)
     except:
         return False
     return True
 
 
-def main():
-    domain = argv[1]
-    discovered = []
-    servers = []
-
+def srv_lookup(domain):
+    hosts = {}
     for name in NAMES:
         for thing in srvlookup.lookup(name, domain=domain):
-            if ldap_ping(get_ip(thing.host)):
-                discovered.append(thing)
+            host_ip = get_ip(thing.host)
+            if ldap_ping(host_ip):
+                hosts[thing.host] = host_ip
+    return hosts
 
-    servers.extend([srv.host for srv in discovered])
-    servers = sorted(list(set(servers)))
-    print(','.join(servers))
+
+def sort_pings(hosts):
+    ping_times = {}
+    for hostname, host_ip in hosts.items():
+        res = check_output(['ping', '-c', '1', host_ip])
+        match = re.search('time=\d+\.\d+', res, re.MULTILINE)
+        if match:
+            part = match.group()
+            rtime = part.split('=').pop()
+            ping_times[hostname] = float(rtime)
+            # print('{}\t{}\t{}'.format(rtime, host_ip, hostname))
+    return ping_times
+
+
+def get_winners(pings, winner_count=2):
+    winners = []
+    for key, value in sorted(pings.items(), key=lambda (k, v): (v, k)):
+        winners.append(key)
+        if len(winners) == winner_count:
+            return winners
+
+
+def main():
+    domain = argv[1]
+    try:
+        host_count = int(argv[2])
+    except:
+        host_count = 2
+    hosts = srv_lookup(domain)
+    host_pings = sort_pings(hosts)
+    winners = get_winners(host_pings, host_count)
+    print(','.join(winners))
 
 
 if __name__ == '__main__':
